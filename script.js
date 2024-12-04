cbae07cb77c324d61fbe98252090b44a4b0020fc
Inditxtlab <indirramirez@gmail.com>
2024-12-04 15:41:09 +0100

    Le Projet First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//BOUTONS//
+const acheterButton = document.getElementById('acheterButton');
+
+// Añadir un evento al botón para cuando se haga clic
+acheterButton.addEventListener('click', function() {
+    alert("Merci pour votre achat");
+})
+//
+console.log(acheterButton);
